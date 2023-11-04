41d36b1632e5cab616611f9d2fd73f6db7f7c5ac
Raqfranca <raqfranca@hotmail.com>
2023-11-03 23:05:00 -0300

    Atualizações

--- aula01/anotacoes.js
+++ aula01/anotacoes.js
@@ -1,5 +1,77 @@
+/* Tipod de dados em JS */
+
 /*
-Os operadores de adição e subtração tem duas formas abreviadas:
+var
+
+    Declara uma variável, opcionalmente, inicializando-a com um valor.
+Experimental let
+
+    Declara uma variável local de escopo do bloco, opcionalmente, inicializando-a com um valor.
+Experimental const
+
+    Declara uma constante de escopo de bloco, apenas de leitura.
+
+*/
+
+var exibeMensagem = function() {
+    if(true) { 
+        var escopoFuncao = 'Caelum'; 
+        let escopoBloco = 'Alura';
+
+       console.log(escopoBloco); // Alura 
+   } 
+   console.log(escopoFuncao); // Caelum 
+   console.log(escopoBloco); 
+}
+
+// String - Todo texto é uma string, mas outro valores também poder ser se os coloquar entre aspas simples.  
+
+var status = 'concluido';
+var numeroGol = '17';
+var cifrao = '$'
+
+// Number - Declaração de número, não precisa ter aspas, esse consjunto inclui os números inteiros 
+
+var number1 = 1;
+var number2 = 2;
+var number3 =46654789; 
+
+// Float number - São os número com casas decimais, também não precisa de aspas para declarar.
+
+var numberFloat = 5.5;
+
+// Boolena - Representa os valores verdadeiro (true) ou falso (false). Para declarar, usar as palavras em inglês sem aspas
+
+var valorVerdeiro = true;
+var valorFalso = false;
+ 
+// Array - Estrutura que permite armazenar vários valores em um único referência.
+
+let myVariable = [1,'Bob','Steve',10]
+
+/*Object - são como uma espécie de "super variáveis" que armazenam uma "coleção de valores" referenciados 
+por nome, e que podem ser recuperados para serem utilizados em diversas outras partes de um programa. 
+
+                    *******Em JavaScript qualquer tipo de dado é um objeto.*******
+
+Cada item dessa "coleção de valores", é chamado de propriedade. 
+Cada propriedade é composta por um par de "nome: valor". 
+Quando uma propriedade armazena uma função, ela se torna o que chamamos de MÉTODO.
+*/
+
+// Exemplo de um objeto literal: 
+
+    var album = {
+      title: "Renaissance",
+      artist: "Beyoncé",
+      released: 2022,
+      showInfo: function() {
+        console.log("Título do álbum: " + this.title + " Lançado em: " + this.released);
+      }
+    };
+/*
+Os operadores de adição e subtração tem duas formas abreviadas, também não precisa de aspas para declarar
+
 
 var soma = 0;
 soma++;
--- aula01/desafio.js
+++ aula01/desafio.js
@@ -8,18 +8,18 @@ myvar = 10;
 
 // Declare uma nova variável chamada `soma`, e adicione uma instrução somando os valores 15 e 8.
 
-var soma = 15 + 8;
+var sum = 15 + 8;
 
 // Atribua à variável `soma` todo o valor dela, somando 1, usando o operador de soma abreviado.
 // Está errado fazer soma = ++soma, essa abreviação faz isso (soma = soma + 1)  
-++soma;
+++sum;
 
 // Atribua à variável `soma` todo o valor dela, multiplicando por 3, usando o operador de multiplicação abreviado.
 //A abreviação faz isso "soma = soma * 3"; 
 
-soma *=3;
+sum *=3;
 
-console.log(soma)
+console.log(sum)
 
 // Qual é o valor da variável `soma` até aqui?
 
@@ -27,7 +27,7 @@ console.log(soma)
 
 // Declare uma variável chamada `souninja`, atribuindo à ela o valor booleano que representa `verdadeiro`.
 
-var souninja = true;
+var souNinja = true;
 
 // Declare uma variável chamada `comida` que recebe um array com os valores 'arroz', 'feijão' e 'ovo'.
 
@@ -39,11 +39,11 @@ console.log(comida[1]);
 
 // Digite o código que verifica se a variável `soma' é igual a variável `myvar` (testando também o tipo).
 
-console.log(soma === myvar);
+console.log(sum === myvar);
 
 // Digite o código que verifica se a variável `myvar` é menor ou igual à variável `soma`.
 
-console.log(myvar <= soma);
+console.log(myvar <= sum);
 
 // Crie uma função chamada `divisao` que receba como parâmetro dois números, e retorne o resultado da divisão entre eles.
 
