ab45fbcdfff08c26344d537aa9d980102747f520
raqfranca <raqfranca@hotmail.com>
2024-01-22 11:54:58 -0300

    Implementing Challenge Functions 29

--- aulaFinal/script.js
+++ aulaFinal/script.js
@@ -1,64 +1,81 @@
 (function(DOM) {
     'use strict';
-      
-    function app(){
+  
+    function app() {
       return {
-        init: function(){
-          console.log("Teste do init");
-          this.companyInfo();
-          this.setupForm();
+        init: function() {
+            this.companyInfo();
+            this.initForm();
         },
   
         companyInfo: function companyInfo() {
-          var self = this; 
+            var self = this;
   
-          var ajax = new XMLHttpRequest();
-          ajax.open('GET', './company.json', true);
-          ajax.send();
-          ajax.addEventListener('readystatechange', function() {
-            self.getCompanyInfo(ajax);
-          }, false);
+            var ajax = new XMLHttpRequest();
+            ajax.open('GET', './company.json', true);
+            ajax.send();
+            ajax.addEventListener('readystatechange', function() {
+                self.getCompanyInfo(ajax);
+            }, false);
         },
   
         getCompanyInfo: function getCompanyInfo(ajax) {
-          if (ajax.readyState === 4 && ajax.status === 200) {
-            var data = JSON.parse(ajax.responseText);
-            var $companyName = new DOM('[data-js="company-name"]');
-            var $companyPhone = new DOM('[data-js="company-phone"]');
-            $companyName.get()[0].textContent = data.name;
-            $companyPhone.get()[0].textContent = data.phone;
+            if (ajax.readyState === 4 && ajax.status === 200) {
+                var data = JSON.parse(ajax.responseText);
+                var $companyName = new DOM('[data-js="company-name"]');
+                var $companyPhone = new DOM('[data-js="company-phone"]');
+                $companyName.get()[0].textContent = data.name;
+                $companyPhone.get()[0].textContent = data.phone;
           }
         },
   
-        setupForm: function setupForm() {
-          var self = this;
-          var form = new DOM('[data-js="form-register"]').get()[0];
+        initForm: function initForm() {
+            var self = this;
+            var $form = new DOM('[data-js="form-register"]').get()[0];
   
-          form.addEventListener('submit', function(event) {
-            event.preventDefault();
-            self.handleFormSubmit(form);
-          });
+            $form.addEventListener('submit', function(event) {
+                event.preventDefault();
+                self.addRowToTable();
+            });
         },
   
-        handleFormSubmit: function handleFormSubmit(form) {
-          var $tableBody = new DOM('table tbody');
-          var newRow = $tableBody.create('tr');
-          
-          
-          var marcaInput = form.querySelector('#marca');
-          var newCell = newRow.create('td');
-          newCell.textContent = marcaInput.value;
+        addRowToTable: function addRowToTable() {
+            var $table = new DOM('table').get()[0];
+            var rowData = this.getFormData();
   
           
-          $tableBody.get()[0].appendChild(newRow);
+            var newRow = $table.insertRow(-1);
   
           
-          form.reset();
+            var imgCell = newRow.insertCell();
+            var cardImg = document.createElement('img');
+            cardImg.src = rowData.urlImg;
+            cardImg.classList.add('card-img-top', 'table-img');
+            imgCell.appendChild(cardImg);
+            
+            Object.values(rowData).forEach(function(value) {
+                if (value !== rowData.urlImg) {
+                var cell = newRow.insertCell();
+                var text = document.createTextNode(value);
+                cell.appendChild(text);
+                }
+            });
+        },
+  
+        getFormData: function getFormData() {
+            var formData = {};
+            var formElements = new DOM('[data-js="form-register"] input').get();
+  
+            formElements.forEach(function(input) {
+                formData[input.id] = input.value;
+            });
+  
+            return formData;
         }
-  
       };
     }
   
     app().init();
   
   })(window.DOM);
+  
